6d26c8da3a6efc0817d95abd947c5544ea093685
alexsandersilv <alexssilva.contato@gmail.com>
2024-02-20 18:21:36 -0300

    Rewriting program && add SpectreConsole

--- GuessNumber.cs
+++ GuessNumber.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,74 +7,50 @@ using System.Threading.Tasks;
 
 namespace GuessNumber
 {
-    internal class GuessNumber
+    class GuessNumber
     {
 
-        static int x, y, z;
+        Random rng = new Random();
+        int x, y, z;
 
-        static Random rng = new Random();
-
-        public static void StartGame()
-        {
-            GenerateGandomNumbers();
-            ShowCalc();
-            InputControll.HandleUserResponse();
-        }
-
-        public static void Banner()
-        {
-            Console.WriteLine("   ______                        _   __                __             " +
-                "\n  / ____/_  _____  __________   / | / /_  ______ ___  / /_  ___  _____" +
-                "\n / / __/ / / / _ \\/ ___/ ___/  /  |/ / / / / __ `__ \\/ __ \\/ _ \\/ ___/" +
-                "\n/ /_/ / /_/ /  __(__  |__  )  / /|  / /_/ / / / / / / /_/ /  __/ /    " +
-                "\n\\____/\\__,_/\\___/____/____/  /_/ |_/\\__,_/_/ /_/ /_/_.___/\\___/_/");
-            Console.WriteLine("Options:\n1-Start Game\n2-Help");
-        }
-
-        public static void Help()
-        {
-            Console.WriteLine("\n=======================================================================================");
-            Console.WriteLine("The value of x must be discovered using simple mathematics");
-            Console.WriteLine("Example:");
-            Console.WriteLine("     X - 43 = 90");
-            Console.WriteLine("");
-            Console.WriteLine("You need to take the total value (90) and add it to the value of the second operand (43)");
-            Console.WriteLine("R: 113");
-            Console.WriteLine("113 - 43 = 90");
-        }
-
-        public static void GenerateGandomNumbers()
+        private void GenerateNumbers()
         {
             x = rng.Next(999);
             y = rng.Next(999);
-            GetValueTotal();
-        }
 
-        static void GetValueTotal()
-        {
             z = x + y;
         }
 
-
-        public static void ShowCalc()
+        public void ShowCalc()
         {
-            Console.WriteLine($"X - {y} = {z}");
+            AnsiConsole.MarkupLine($"[yellow1]X - {y} = {z}[/]");
         }
 
-        public static void HandleResponse(int userResponse)
+        public void HandleResponse()
         {
-            if (userResponse == x)
-            {
-                Console.WriteLine("Correct!");
-                Console.WriteLine("For now that's it (=");
-            }
-            else 
-            {
-                Console.WriteLine("Incorrect");
-                Console.WriteLine("Try again");
-                InputControll.HandleUserResponse();
+            int response = AnsiConsole.Ask<int>("What is the value of [yellow1]X[/]?: ");
 
+            if (response == x)
+            {
+                AnsiConsole.MarkupLine($"[green]Correct, you got the value of[/] [green1]X ({x})[/] [green]right[/]");
+                AnsiConsole.MarkupLine($"[green1]Next level[/]");
+
+                StartGame();
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red1]Incorrect answer[/]");
+                HandleResponse();
             }
         }
+
+        public void StartGame()
+        {
+            GenerateNumbers();
+            ShowCalc();
+            HandleResponse();
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,58 @@ namespace GuessNumber
 
         static void Main(string[] args)
         {
-            GuessNumber.Banner();
-            InputControll.GetUserInputMenu();
+            AnsiConsole.Write(
+                new FigletText("Guess Number")
+                .Centered()
+                .Color(Color.Blue)
+                );
+
+
+            string menuOption = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("  Game Options")
+                .PageSize(10)
+                .AddChoices(new[] {
+                    "Start Game", "Help", "Exit",
+                }));
+
+
+            switch (menuOption)
+            {
+                case "Start Game":
+                    new GuessNumber().StartGame();
+                    
+                    break;
+                case "Help":
+                    var rule = new Rule("[green1]Instructions[/]");
+                    rule.Style = Style.Parse("lime");
+                    AnsiConsole.Write(rule);
+
+                    AnsiConsole.WriteLine(" The value of x must be discovered using simple mathematics");
+                    AnsiConsole.WriteLine(" You need to take the total value (90) and add it to the value of the second operand (43)");
+                    AnsiConsole.WriteLine(" Example: X - 43 = 90");
+                    AnsiConsole.WriteLine(" R: 113");
+                    AnsiConsole.WriteLine(" 113 - 43 = 90");
+                    AnsiConsole.Write(rule);
+
+                    string option = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("")
+                        .PageSize(10)
+                        .AddChoices(new[] {
+                            "OK", "Exit",
+                        }));
+
+                    break;
+
+                case "Exit":
+
+                break;
+                   
+            }
+
+
+
         }
     }
 }
